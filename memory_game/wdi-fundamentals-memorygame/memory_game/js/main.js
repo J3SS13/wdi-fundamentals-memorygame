1ea057abf1c2d94df4213e63498241f6aaf70df7
J3SS13 <33236077+J3SS13@users.noreply.github.com>
2018-09-14 09:27:40 -0400

    Changed computers
    new game logic

---
+++ memory_game/wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -0,0 +1,61 @@
+
+
+
+
+var cards = [
+
+{
+	rank : "queen", 
+	suit : "hearts",
+	cardImage : "images/queen-of-hearts.png"
+},
+{
+	rank : "queen",
+	suit : "diamonds",
+	cardImage : "images/queen-of-diamonds.png"
+},
+
+{
+	rank : "king", 
+	suit : "hearts",
+	cardImage : "images/king-of-hearts.png"
+},
+
+{
+	rank : "king",
+	suit : "diamonds",
+	cardImage : "images/king-of-diamonds.png"
+}
+
+];
+
+
+
+
+var cardsInPlay = [];
+
+var checkForMatch = function() { 
+};
+
+var flipCard = function(cardId) {
+
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
+checkForMatch();
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+}; 
+
+flipCard(0);
+flipCard(2);
+
+
